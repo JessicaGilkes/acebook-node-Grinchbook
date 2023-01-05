3f2331068b62e6ee832c4e248ac4780bdf6d2bf0
Samir70 <sbetmouni@talktalk.net>
2023-01-05 14:19:45 +0000

    make emails unique in tests

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -2,7 +2,7 @@ describe("Registration", () => {
   it("A user signs up and is redirected to sign in", () => {
     // sign up
     cy.visit("/users/new");
-    cy.get("#email").type("someone@example.com");
+    cy.get("#email").type("someone@signupspec.com");
     cy.get("#password").type("password");
     cy.get("#username").type("someone1");
     cy.get("#submit").click();

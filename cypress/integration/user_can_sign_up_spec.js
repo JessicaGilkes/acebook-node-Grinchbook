2c0c76bd7164c6a995964e7e7a90b87458813af0
Jess <gilkes.jessica@gmail.com>
2023-01-05 15:42:00 +0000

    each sign ups unique

--- cypress/integration/user_can_sign_up_spec.js
+++ cypress/integration/user_can_sign_up_spec.js
@@ -12,4 +12,24 @@ describe("Registration", () => {
 
     cy.url().should("include", "/sessions/new");
   });
+
+  it("A user can't sign up with email already in use", () => {
+    // sign up
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@signupspec.com");
+    cy.get("#password").type("password");
+    cy.get("#username").type("someone1");
+    cy.get("#submit").click();
+
+    cy.visit("/users/new");
+    cy.get("#email").type("someone@signupspec.com");
+    cy.get("#password").type("password");
+    cy.get("#username").type("someone1");
+    cy.get("#submit").click();
+
+    cy.url().should("include", "/users");
+    cy.get("#email-error").should("contain", "Email already in use, try another!");
+  });
 });
+
+

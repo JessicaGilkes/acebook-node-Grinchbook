2a627f80b4e305d2d662440e861342e13b2bbd86
Samir70 <sbetmouni@talktalk.net>
2023-01-05 17:10:52 +0000

    remove some console logs

--- controllers/resetdb.js
+++ controllers/resetdb.js
@@ -1,13 +1,11 @@
 const User = require("../models/user");
 
 const ResetController = {
-    Reset: async (req,res) => {
-        console.log("reseting");
-       console.log( await User.deleteMany({  }));
-       // const username = req.session.user? req.session.user.username : "valued customer";
-       // res.render("home/index", { title: "Acebook", username, loggedin: req.session.user });
-        res.redirect("/");
-    }
-}
+  Reset: async (req, res) => {
+    console.log("reseting db");
+    await User.deleteMany({});
+    res.redirect("/");
+  },
+};
 
 module.exports = ResetController;

b376d570dd28f41b6abfa3dc25231b2c1d87a92b
Jess <gilkes.jessica@gmail.com>
2023-01-05 11:24:52 +0000

    email in use function

--- controllers/users.js
+++ controllers/users.js
@@ -6,13 +6,25 @@ const UsersController = {
   },
 
   Create: (req, res) => {
-    const user = new User(req.body);
-    user.save((err) => {
-      if (err) {
-        throw err;
+    console.log(req.body);
+
+    const email = req.body.email;
+
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        // email already in use
+        res.status(409).render("users/new",{emailinuse: true});
+      } else {
+        const user = new User(req.body);
+        user.save((err) => {
+          if (err) {
+            throw err;
+          }
+          res.status(201).redirect("/posts");
+        });
       }
-      res.status(201).redirect("/posts");
-    });
+    })
+
   },
 };
 

d1fa728af31d9b6b0677b9ff943fa8473cf5fa51
Samir70 <sbetmouni@talktalk.net>
2023-01-06 14:23:10 +0000

    add seedPosts function

--- models/dbFuncs.js
+++ models/dbFuncs.js
@@ -1,4 +1,4 @@
-// const { ObjectID } = require("mongodb");
+const { ObjectID } = require("mongodb");
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://0.0.0.0/acebook_test", {
   useNewUrlParser: true,
@@ -17,11 +17,13 @@ const dropPosts = async () => {
   await Post.deleteMany({});
 };
 
+const kirkID = ObjectID("63b810bd87affe92a654e712");
 const users = [
   {
     email: "kirk@original.com",
     password: "finalFrontier",
     username: "James T Kirk",
+    _id: kirkID,
   },
   { email: "picard@ng.com", password: "makeItSo", username: "Jean Luc" },
   { email: "sisco@ds9.com", password: "emissary", username: "Benjamin Sisko" },
@@ -41,4 +43,40 @@ const seedUsers = async () => {
   }
 };
 
-module.exports = { dropUsers, dropPosts, seedUsers };
+const posts = [
+  {
+    message: "Space: the final frontier",
+    likes: 328,
+    author: kirkID,
+    date: new Date("1966-09-06"),
+  },
+  {
+    message: "These are the voyages of the starship.... Enterprise.",
+    likes: 209,
+    author: kirkID,
+    date: new Date("1966-09-13"),
+  },
+  {
+    message:
+      "It's five year mission to explore new worlds and new civilisations.",
+    likes: 199,
+    author: kirkID,
+    date: new Date("1966-09-20"),
+  },
+];
+
+const seedPosts = async () => {
+  await dropPosts();
+  for (let p = 0; p < posts.length; p++) {
+    const newPost = new Post(posts[p]);
+    console.log({ newPost });
+    newPost.save((err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("saved post: " + newPost.message)
+    });
+  }
+};
+
+module.exports = { dropUsers, dropPosts, seedUsers, seedPosts };

21e24787938ddc34f097e0fe3335a3608d5ea73b
Samir70 <sbetmouni@talktalk.net>
2023-01-06 15:00:25 +0000

    run seedusers before trying to sign in

--- cypress/integration/user_can_see_likes_count_on_post.js
+++ cypress/integration/user_can_see_likes_count_on_post.js
@@ -1,13 +1,14 @@
 describe("Timeline", () => {
   it("can see likes count on an old post", () => {
-    cy.task("seedPosts", "seeLikesCountOnPost")
     // sign in
+    cy.task("seedUsers", "seeLikesCountOnPost")
     cy.visit("/sessions/new");
     cy.get("#email").type("picard@ng.com");
     cy.get("#password").type("makeItSo");
     cy.get("#submit").click();
-
+    
     // Assert that we can see the likes count
+    cy.task("seedPosts", "seeLikesCountOnPost")
     cy.get(".posts").should("contain", "328 likes");
   });
 });

2b093bd14a18bdb576c8d0aeb8049d9501cdd2de
Samir70 <sbetmouni@talktalk.net>
2023-01-06 15:11:53 +0000

    update tests for user signing in

--- cypress/integration/user_can_sign_in_spec.js
+++ cypress/integration/user_can_sign_in_spec.js
@@ -1,16 +1,20 @@
 describe("Authentication", () => {
+  it("A user who is not signed in cannot reach the posts page", () => {
+    cy.visit("/posts");
+    cy.url().should("include", "sessions/new");
+    cy.contains("h1", "Login");
+  });
+  it("A user who is not signed in cannot reach the posts/new page", () => {
+    cy.visit("/posts/new");
+    cy.url().should("include", "sessions/new");
+    cy.contains("h1", "Login");
+  });
   it("A user signs in and is redirected to /posts", () => {
-    // sign up
-    cy.visit("/users/new");
-    cy.get("#email").type("someone@signinspec.com");
-    cy.get("#password").type("password");
-    cy.get("#username").type("signinTester");
-    cy.get("#submit").click();
-
     // sign in
+    cy.task("seedUsers", "cansignInSpec");
     cy.visit("/sessions/new");
-    cy.get("#email").type("someone@signinspec.com");
-    cy.get("#password").type("password");
+    cy.get("#email").type("picard@ng.com");
+    cy.get("#password").type("makeItSo");
     cy.get("#submit").click();
 
     cy.url().should("include", "/posts");

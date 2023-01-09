128af94f5a2771da9474ef3b6b6f150a67e53884
Samir70 <sbetmouni@talktalk.net>
2023-01-09 16:05:32 +0100

    add a friends field to users

--- models/user.js
+++ models/user.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
+const { ObjectID } = require("mongodb");
 
 const UserSchema = new mongoose.Schema({
   email: String,
   password: String,
   username: String,
+  friends: [ObjectID],
 });
 
 const User = mongoose.model("User", UserSchema);

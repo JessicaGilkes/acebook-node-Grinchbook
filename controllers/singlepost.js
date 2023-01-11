b3254acfbf8e297991c2503285784be439600d18
Craig Donald <1craig.donald@gmail.com>
2023-01-11 12:21:45 +0000

    Like and Unlike working

--- controllers/singlepost.js
+++ controllers/singlepost.js
@@ -11,15 +11,37 @@ const SinglePostController = {
       .populate("author")
       .populate("likes.voters")
       .then((post) => {
-        console.log(post);
+        let isLiked = post.likes.voters.map(v => v._id).indexOf(req.session.user._id) != -1
+        console.log(isLiked);
+        console.log(post.likes.voters.indexOf(req.session.user._id));
         res.render("singlepost/index", {
           post,
           author: post.author.username,
           loggedin: req.session.user,
           likescount: post.likes.count,
-          voters: post.likes.voters.map(v => v.username).join(", ")
+          voters: post.likes.voters.map(v => v.username).join(", "),
+          isLiked
         });
       });
+  },
+  Unlike: (req, res) => {
+    Post.updateOne(
+      { _id: req.body.post_id },
+      { $inc: { "likes.count": -1 }, $pull: { "likes.voters": req.session.user._id }},
+      () => {
+        res.redirect("/singlepost?id="+ req.body.post_id);
+      }
+    );
+  },
+  Like: (req, res) => {
+    console.log("SinglePostController.like", req.body)
+    Post.updateOne(
+      { _id: req.body.post_id },
+      { $inc: { "likes.count": 1 }, $push: { "likes.voters": req.session.user._id }},
+      () => {
+        res.redirect("/singlepost?id="+ req.body.post_id);
+      }
+    );
   },
 };
 

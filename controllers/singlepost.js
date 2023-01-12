c458579994df95ea2089c6d086d1114c3387d10b
Jess <gilkes.jessica@gmail.com>
2023-01-12 17:06:15 +0000

    delete button

--- controllers/singlepost.js
+++ controllers/singlepost.js
@@ -13,41 +13,75 @@ const SinglePostController = {
       .populate("likes.voters")
       .populate("comments.user_id")
       .then((post) => {
-        post.comments.forEach(c => {
-          console.log("SinglePostController.Index: getting dateString for ", c.content)
-          c.dateString = timeStamp(c.date)
-          console.log("gave dateString: ", c.dateString)
-        })
-        let isLiked = post.likes.voters.map(v => v._id).indexOf(req.session.user._id) != -1
+        post.comments.forEach((c) => {
+          console.log(
+            "SinglePostController.Index: getting dateString for ",
+            c.content
+          );
+          c.dateString = timeStamp(c.date);
+          console.log("gave dateString: ", c.dateString);
+        });
+        let isLiked =
+          post.likes.voters.map((v) => v._id).indexOf(req.session.user._id) !=
+          -1;
         res.render("singlepost/index", {
           post,
           author: post.author,
           loggedin: req.session.user,
           likescount: post.likes.count,
-          voters: post.likes.voters.map(v => v.username).join(", "),
-          isLiked
+          voters: post.likes.voters.map((v) => v.username).join(", "),
+          isLiked,
         });
       });
   },
   Unlike: (req, res) => {
     Post.updateOne(
       { _id: req.body.post_id },
-      { $inc: { "likes.count": -1 }, $pull: { "likes.voters": req.session.user._id }},
+      {
+        $inc: { "likes.count": -1 },
+        $pull: { "likes.voters": req.session.user._id },
+      },
       () => {
-        res.redirect("/singlepost?id="+ req.body.post_id);
+        res.redirect("/singlepost?id=" + req.body.post_id);
       }
     );
   },
   Like: (req, res) => {
-    console.log("SinglePostController.like", req.body)
+    console.log("SinglePostController.like", req.body);
     Post.updateOne(
       { _id: req.body.post_id },
-      { $inc: { "likes.count": 1 }, $push: { "likes.voters": req.session.user._id }},
+      {
+        $inc: { "likes.count": 1 },
+        $push: { "likes.voters": req.session.user._id },
+      },
       () => {
-        res.redirect("/singlepost?id="+ req.body.post_id);
+        res.redirect("/singlepost?id=" + req.body.post_id);
       }
     );
   },
+  Delete: (req, res) => {
+    Post.findById(req.body.post_id)
+      .populate("author")
+      .then((post) => {
+        if (!post) {
+          res.status(404).json({ postnotfound: "No such post" + req.body.post_id});
+        }
+        console.log("found", post.author._id, req.session.user._id, post.author._id == req.session.user._id)
+        if (post.author._id != req.session.user._id) {
+          return res.status(401).json({ notauthorized: "User not authorized" });
+        }
+        post
+          .remove()
+          .then(() => res.redirect("/posts"))
+          .catch((err) => {
+            console.log(err);
+            res.status(404).json({ postnotfound: "Post not found 1" });
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(404).json({ postnotfound: "Post not found" });
+      });
+  },
 };
-
 module.exports = SinglePostController;
--- routes/singlepost.js
+++ routes/singlepost.js
@@ -6,6 +6,7 @@ const SinglePostController = require("../controllers/singlepost")
 router.get("/", SinglePostController.Index)
 router.post("/like", SinglePostController.Like)
 router.post("/unlike", SinglePostController.Unlike)
+router.post("/delete", SinglePostController.Delete)
 // router.get("#", SinglePostController.Modal)
 
 module.exports = router;

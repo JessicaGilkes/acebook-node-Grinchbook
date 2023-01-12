a2a0386dd94292cd116d26df72d4409ba8d6f156
Craig Donald <1craig.donald@gmail.com>
2023-01-12 12:26:55 +0000

    Post tests v2

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -1,14 +1,14 @@
 describe("Timeline", () => {
   it("can submit posts, when signed in, and view them", () => {
     // sign in
-    cy.task("seedUsers", "userCanSubmitPost");
+    cy.task("seedUsers", "userCanSubmitPost")
     cy.visit("/sessions/new");
     cy.get("#email").type("picard@ng.com");
     cy.get("#password").type("makeItSo");
     cy.get("#submit").click();
-
+    
     // submit a post
-    cy.task("seedPosts", "userCanSubmitPost");
+    cy.task("seedPosts", "userCanSubmitPost")
     cy.visit("/posts");
     cy.contains("create new post").click();
 
@@ -17,4 +17,45 @@ describe("Timeline", () => {
 
     cy.get(".posts").should("contain", "Hello, world!");
   });
+
+  it("can submit an image along with a post", () => {
+    // sign in
+    cy.task("seedUsers", "userCanSubmitPost")
+    cy.visit("/sessions/new");
+    cy.get("#email").type("picard@ng.com");
+    cy.get("#password").type("makeItSo");
+    cy.get("#submit").click();
+    
+    // submit a post
+    cy.task("seedPosts", "userCanSubmitPost")
+    cy.visit("/posts");
+    cy.contains("create new post").click();
+
+    cy.get("#message").type("Hello, here's a happy coo!");
+    cy.get("#photo").type("https://oldmooresalmanac.com/wp-content/uploads/2017/11/cow-2896329_960_720-Copy.jpg")
+    cy.get("#new-post-form").submit();
+
+    cy.get(".posts").should("contain", "Hello, here's a happy coo!");
+    cy.get(".imgupload").should("have.attr", "src").should("include", "https://oldmooresalmanac.com/wp-content/uploads/2017/11/cow-2896329_960_720-Copy.jpg")
+  });
+
+  // Shouldn't work, but does, possibly due to Cypress' browser
+  //
+  // it("can submit an image as a post", () => {
+  //   // sign in
+  //   cy.task("seedUsers", "userCanSubmitPost")
+  //   cy.visit("/sessions/new");
+  //   cy.get("#email").type("picard@ng.com");
+  //   cy.get("#password").type("makeItSo");
+  //   cy.get("#submit").click();
+    
+  //   // submit a post
+  //   cy.task("seedPosts", "userCanSubmitPost")
+  //   cy.visit("/posts");
+  //   cy.contains("create new post").click();
+
+  //   cy.get("#photo").type("https://live.staticflickr.com/2275/2366822806_5dd2352131.jpg");
+  //   cy.get("#new-post-form").submit();
+  //   cy.get(".imgupload").should("have.attr", "src").should("include", "https://live.staticflickr.com/2275/2366822806_5dd2352131.jpg");
+  // });
 });
